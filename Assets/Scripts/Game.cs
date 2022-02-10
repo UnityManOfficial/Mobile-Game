7945704e73c39007eec6029950c04441270406da
Unity Man <40305575+UnityManOfficial@users.noreply.github.com>
2022-02-10 10:26:59 -0800

    Update V0.7
    Added Loading and fade to game UI
    Added Difficulty script placeholder

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    [SerializeField] [Range(1, 3)] int Difficulty = 1;
+
+    void Awake()
+    {
+        dontDestroy();
+    }
+
+    private void dontDestroy()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    DifficultyChanger();
+    }
+
+    private void DifficultyChanger()
+    {
+
+    }
+
+
+
+}
