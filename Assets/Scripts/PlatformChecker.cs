801771bab7481b610fe53883c035735592517b99
Unity Man <40305575+UnityManOfficial@users.noreply.github.com>
2022-02-11 09:02:26 -0800

    Update V0.8
    Added Pause Menu
    Added HealthBar Placeholder

--- Assets/Scripts/PlatformChecker.cs
+++ Assets/Scripts/PlatformChecker.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlatformChecker : MonoBehaviour
 {
     public GameObject Touch;
+    public GameObject PauseMenu;
+    public GameObject Fade;
+    public GameObject Loading;
+    private bool GamePaused = false;
 
+    Animator MyAnimatorFade;
 
     public void Start()
     {
+        MyAnimatorFade = Fade.GetComponent<Animator>();
 #if UNITY_STANDALONE_WIN
         Touch.SetActive(false);
 
@@ -19,5 +26,51 @@ public class PlatformChecker : MonoBehaviour
         Touch.SetActive(true);
 #endif
 
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && GamePaused == false)
+        {
+            GamePaused = true;
+            if (GamePaused)
+            {
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+        else if (Input.GetButtonDown("Cancel") && GamePaused == true)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            GamePaused = false;
+        }
+    }
+
+    public void MenuOpen()
+    {
+        StartCoroutine(MenuStart());
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Unpause()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    IEnumerator MenuStart()
+    {
+        Time.timeScale = 1f;
+        MyAnimatorFade.SetBool("Go", true);
+        yield return new WaitForSeconds(1);
+        MyAnimatorFade.SetBool("Go", false);
+        SceneManager.LoadScene("Main Menu");
     }
 }
